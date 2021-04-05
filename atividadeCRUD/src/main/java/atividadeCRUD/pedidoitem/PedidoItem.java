b97fc1207a497bcdee66e9ad4a0f723322a66d97
Jaguanhara Neto <51683220+Jaguanhara@users.noreply.github.com>
2021-04-04 22:57:00 -0300

    Update PedidoItem.java

--- atividadeCRUD/src/main/java/atividadeCRUD/pedidoitem/PedidoItem.java
+++ atividadeCRUD/src/main/java/atividadeCRUD/pedidoitem/PedidoItem.java
@@ -6,20 +6,33 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.springframework.util.StringUtils;
+
+import atividadeCRUD.Pedido.Pedido;
+import atividadeCRUD.Produtos.Produto;
+
 
 @Entity
 public class PedidoItem {
 	
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	private int quantidade;
+	private String quantidade;
 	
+	@ManyToOne
+	private Pedido pedido;
+	
+	@ManyToOne
+	private Produto produto;
+	
+
 	@Deprecated
 	public PedidoItem() {}
 	
-	public PedidoItem (int quantidade) {
+	public PedidoItem (String quantidade) {
 		this.quantidade = quantidade;
 	}
 	public Long getId() {
@@ -28,12 +41,29 @@ public class PedidoItem {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public int getQuantidade() {
+	public String getQuantidade() {
 		return quantidade;
 	}
-	public void setQuantidade(int quantidade) {
+	public void setQuantidade(String quantidade) {
 		this.quantidade = quantidade;
 	}
+	
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -50,6 +80,8 @@ public class PedidoItem {
 		return Objects.equals(id, other.id);
 	}
 	
-	
+	public boolean isVazio() {
+		return !StringUtils.hasText(quantidade);
+	}
 	
 }	
