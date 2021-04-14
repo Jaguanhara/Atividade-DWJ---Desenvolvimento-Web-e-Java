08697daf6009da0f99e84e3cae2e80b53c8b0e3c
Jaguanhara Neto <51683220+Jaguanhara@users.noreply.github.com>
2021-04-13 21:57:35 -0300

    Update Produto.java

--- atividadeCRUD/src/main/java/atividadeCRUD/Produto/Produto.java
+++ atividadeCRUD/src/main/java/atividadeCRUD/Produto/Produto.java
@@ -1,4 +1,4 @@
-package atividadeCRUD.Produtos;
+package atividadeCRUD.Produto;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,7 +27,7 @@ public class Produto {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	@NotBlank(message = "Campo não pode ser vazio")
+	@NotBlank(message = "Campo nÃ£o pode ser vazio")
 	private String nomeprod;
 
 	@NotNull()
